0c06e265a310937d191455769ca0a367b2b7ba9d
Kay Thwe Min Han <kthweminhan@gmail.com>
2026-04-20 22:13:55 +0900

    input has added

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,17 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
 
+    // Prints a message to request input
+    println("Enter any word: ")
+
+// Reads and stores the user input. For example: Happiness
+    val yourWord = readln()
+
+// Prints a message with the input
+    print("You entered the word: ")
+    print(yourWord)
+// You entered the word: Happiness
+
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
