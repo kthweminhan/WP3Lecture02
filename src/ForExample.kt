7679496288fafe5a6281712f2803913d675e006c
Kay Thwe Min Han <kthweminhan@gmail.com>
2026-04-21 07:38:21 +0900

    for loop test with list of varibles

--- src/ForExample.kt
+++ src/ForExample.kt
@@ -1,10 +1,13 @@
 class ForExample {
-    fun main() {
-
+    fun runExample() {
         val items = listOf("apple", "banana", "kiwifruit")
         for (item in items) {
             println(item)
         }
-
     }
+}
+
+fun main() {   // ✅ entry point
+    val obj = ForExample()
+    obj.runExample()
 }
